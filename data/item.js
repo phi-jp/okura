a8b5fbedfa6bf20da7d6c8c0935b2f607b2af651
daishihmr <daishi.hmr@gmail.com>
2014-01-09 17:52:53 +0900

    add item schema

---
+++ data/item.js
@@ -0,0 +1,16 @@
+require('./connect');
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var ItemSchema = new Schema({
+    title: { type: String, requried: true },
+    createdAt: { type: Date },
+    updatedAt: { type: Date }
+});
+ItemSchema.pre('save', function(next) {
+    console.log("save Item id:" + this.id);
+    this.updatedAt = new Date();
+    next();
+});
+
+var Item = exports.Item = mongoose.model('Item', ItemSchema);
